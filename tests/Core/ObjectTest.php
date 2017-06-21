14f78d7baacf9e5177edd94895529f684b5ad415
Hiran Milinda <hiran.milinda@au.harveynorman.com>
2017-06-21 15:14:53 +1000

    Path corrected

---
+++ tests/Core/ObjectTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Phpboot\Core\Object;
+
+class ObjectTest extends TestCase
+{
+    public function testObject()
+    {
+        $object = new Object();
+        $this->assertEquals('Phpboot\Core\Object', $object->toString());
+        
+    }
+}
